5b2367fdcf55076370e000e71e0ed66000fa9f47
gongchao92 <42458380+gongchao92@users.noreply.github.com>
2020-10-18 20:18:51 +0800

    Update Test1.java

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -1,5 +1,9 @@
 public class Test1 {
     public static void test1() {
-        System.out.println("test1");
+        System.out.println("test111");
+    }
+    
+    public static void t() {
+        
     }
 }
